ce4a50c5bc3027633e5825a4e613ce54d7a6ce9b
mhaouas <mhaouas@student.42angouleme.fr>
2025-07-03 00:40:04 +0200

    Making multiplayer possible, both players can control their respectives characters.

--- game-jam-42/scripts/Player2.gd
+++ game-jam-42/scripts/Player2.gd
@@ -5,7 +5,11 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _ready() -> void:
-	$MultiplayerSynchronizer.set_multiplayer_authority(1)
+	var playerId
+	for i in GameManager.Players:
+		if i != 1:
+			playerId = i
+	$MultiplayerSynchronizer.set_multiplayer_authority(playerId)
 
 func _physics_process(delta: float) -> void:
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
